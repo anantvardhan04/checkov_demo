e89940b8cc7e307ebd677c4ada7701e35c2fbdb4
Anant Vardhan <anant.vardhan@nagarro.com>
2022-02-11 16:41:38 +0530

    Changed Instance Type

--- terraform/ec2_instance_size.tf
+++ terraform/ec2_instance_size.tf
@@ -15,7 +15,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "r6.xlarge"
+  instance_type = "r6.2xlarge"
 
   tags = {
     Name = "HelloWorld"
